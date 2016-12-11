f6995b12348446e8d0e004d60d7daaf49fb41d6b
Gerardo Leal <gerard.leal91@gmail.com>
2016-12-10 22:20:58 -0300

    Routing

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
 var express = require('express');
-
 var port = process.env.PORT || 3000;
+var nav = [
+    {
+        link: '/books',
+        text: 'Books'
+    }, {
+        link: '/authors',
+        text: 'Authors'
+    }
+];
 
 var app = express();
+var booksRouter = require('./src/routes/books.routes.js')(nav);
 
 /** Statics */
+/////////////
+
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 /** Routes */
-app.get('/', function(req, res) {
+////////////
+
+app.get('/', function (req, res) {
     res.render('index', {
         title: 'Hello for EJS',
-        list: ['a', 'b']
+        nav: [{
+            link: '/books',
+            text: 'Books'
+        }, {
+            link: '/authors',
+            text: 'Authors'
+        }]
     });
 });
 
-app.get('/books', function(req, res) {
-    res.send('Hello Books');
-});
+app.use('/books', booksRouter);
 
-app.listen(port, function(err) {
+
+/** App Bootstrap */
+///////////////////
+app.listen(port, function (err) {
     console.log('Running Server on port ' + port);
 });
